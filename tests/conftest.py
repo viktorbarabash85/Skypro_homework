4920cac5253abd76fbd1cc4114488b488a809d19
viktor <lav.diz@mail.ru>
2024-10-06 18:31:19 +1100

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+""" Фикстуры для тестирования правильности маскирования номера карты """
+@pytest.fixture
+def card_number_correct():
+    return "1234 56** **** 3456"
+
+@pytest.fixture
+def card_number_incorrect():
+    return "Некорректно введен номер карты"
