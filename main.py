233bb6dd2bfbd76a932a88fd84eacf960aa8c7a5
viktor <lav.diz@mail.ru>
2024-10-17 21:55:34 +1100

    edit main.py (run filter_by_currency transaction_descriptions card_number_generator)

--- main.py
+++ main.py
@@ -11,13 +11,7 @@ transactions = (
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
             "to": "Счет 11776614605963066702"
@@ -26,13 +20,7 @@ transactions = (
             "id": 142264268,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
             "to": "Счет 75651667383060284188"
@@ -41,13 +29,7 @@ transactions = (
             "id": 873106923,
             "state": "EXECUTED",
             "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
+            "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 44812258784861134719",
             "to": "Счет 74489636417521191160"
@@ -56,13 +38,7 @@ transactions = (
             "id": 895315941,
             "state": "EXECUTED",
             "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
             "to": "Visa Platinum 8990922113665229"
@@ -71,13 +47,7 @@ transactions = (
             "id": 594226727,
             "state": "CANCELED",
             "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
             "description": "Перевод организации",
             "from": "Visa Platinum 1246377376343588",
             "to": "Счет 14211924144426031657"
@@ -131,15 +101,23 @@ print("_"*13)
 
 print("Функция принимает на список словарей, представляющих транзакции.")
 print("Возвращает итератор, который поочередно выдает транзакции,")
-print(list(filter_by_currency(transactions, "USD")))
+usd_transactions = filter_by_currency(transactions, "USD")
+for _ in range(2):
+    print(next(usd_transactions))
+# print(list(filter_by_currency(transactions, "USD")))
 print("_"*13)
 
 print("Генератор принимает список словарей с транзакциями")
 print("Возвращает описание каждой операции по очереди.")
-print(list(transaction_descriptions(transactions)))
+descriptions = transaction_descriptions(transactions)
+for _ in range(5):
+    print(next(descriptions))
+# print(list(transaction_descriptions(transactions)))
 print("_"*13)
 
 print("Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX.")
 print("Принимает начальное и конечное значения для генерации диапазона номеров.")
-print(list(card_number_generator(1, 5)))
+# print(list(card_number_generator(1, 5)))
+for card_number in card_number_generator(1, 5):
+    print(card_number)
 print("_"*13)
