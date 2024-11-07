60b82741ca567e71508cfa93d99973c4cf5b95db
viktor <lav.diz@mail.ru>
2024-11-08 09:39:32 +1100

    update masks.py utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,20 @@
 from typing import Union
 
+from src.file_logger import setup_logger
+
+# Настраиваем логгер для модуля masks
+logger = setup_logger("masks", "masks")
+
 
 def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
     """Маскирует номер банковской карты"""
     card_number_str = str(card_number)
-    # if card_number_str.isdigit() and card_number != 0 and len(card_number_str) == 16:
     if card_number_str.isdigit() and int(card_number) != 0 and len(card_number_str) == 16:
-        return " ".join([card_number_str[:4], card_number_str[4:6] + "*" * 2, "*" * 4, card_number_str[-4:]])
+        masked_number = " ".join([card_number_str[:4], card_number_str[4:6] + "*" * 2, "*" * 4, card_number_str[-4:]])
+        logger.info("Успешно замаскирован номер карты.")
+        return masked_number
     else:
+        logger.error("Некорректно введен номер карты.")
         return "Некорректно введен номер карты"
 
 
@@ -15,6 +22,9 @@ def get_mask_account(account_number: Union[int, str]) -> Union[str]:
     """Маскирует номер банковского счета"""
     account_number_str = str(account_number)
     if account_number_str.isdigit() and int(account_number) != 0 and len(account_number_str) == 20:
-        return "*" * 2 + account_number_str[-4:]
+        masked_account = "*" * 2 + account_number_str[-4:]
+        logger.info("Успешно замаскирован номер счета.")
+        return masked_account
     else:
+        logger.error("Некорректно введен номер счета.")
         return "Некорректно введен номер счета"
